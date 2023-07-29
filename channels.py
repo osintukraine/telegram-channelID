f371bb1414b68e7ea1e9b8241fbfe607b6879891
Ben Borges <info@buzzworkers.com>
2023-07-29 09:36:58 +0200

    Update channels.py

--- channels.py
+++ channels.py
@@ -1,7 +1,6 @@
 from telethon.sync import TelegramClient
 from telethon.tl.functions.channels import GetFullChannelRequest
-from telethon.tl.functions.messages import GetDialogsRequest
-from telethon.tl.types import Channel, Chat, User
+from telethon.tl.types import Channel
 import csv
 from dotenv import load_dotenv
 import os
@@ -17,15 +16,15 @@ client = TelegramClient('session_name', api_id, api_hash)
 async def main():
     await client.start(phone)
     dialogs = await client.get_dialogs()
-    channels = [entity for entity in dialogs.entity if isinstance(entity, Channel)]
+    channels = [entity for entity in dialogs if isinstance(entity.entity, Channel)]
     
     data = []
     for channel in channels:
-        full_channel = await client(GetFullChannelRequest(channel))
-        channel_link = f"https://t.me/{channel.username}" if channel.username else "No public link"
+        full_channel = await client(GetFullChannelRequest(channel.entity))
+        channel_link = f"https://t.me/{channel.entity.username}" if channel.entity.username else "No public link"
         followers = full_channel.full_chat.participants_count
-        chat_id = channel.id
-        data.append((channel.title, channel_link, followers, chat_id))
+        chat_id = channel.entity.id
+        data.append((channel.entity.title, channel_link, followers, chat_id))
     
     data.sort()  # Sort by channel name
 
